5edb846def02a62ebd2d85ef5cf5ca18c6822819
KongBoy <s89334roy@yhoo.com.tw>
2024-10-31 17:18:32 +0800

    把 前人古蹟的 dist/img 的東西放進 GEOJSON_file/before/coastal/img, .gitignore更新OK, 更新相關路徑OK

--- GEOJSON_file/before/hotpoint_withJPG.js
+++ GEOJSON_file/before/hotpoint_withJPG.js
@@ -3,9 +3,9 @@ var hotpointWithJPG = [
         name: "台北港",
         latlng: [25.145, 121.334],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/臺北港_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/臺北港_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/臺北港_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/臺北港_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/臺北港_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/臺北港_106.jpg"
         ]
 
     },
@@ -13,108 +13,108 @@ var hotpointWithJPG = [
         name: "烏石港",
         latlng: [24.8624, 121.8347],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/烏石港_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/烏石港_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/烏石港_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/烏石港_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/烏石港_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/烏石港_106.jpg"
         ]
     },
     {
         name: "新竹新豐及頭前溪",
         latlng: [24.8499, 120.929],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/新竹新豐及頭前溪_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/新竹新豐及頭前溪_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/新竹新豐及頭前溪_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/新竹新豐及頭前溪_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/新竹新豐及頭前溪_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/新竹新豐及頭前溪_106.jpg"
         ]
     },
     {
         name: "台南黃金",
         latlng: [22.9546, 120.1567],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/台南黃金_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/台南黃金_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/台南黃金_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/台南黃金_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/台南黃金_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/台南黃金_106.jpg"
         ]
     },
     {
         name: "臺東縣南迴公段",
         latlng: [22.3278, 120.9145],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/臺東縣南迴公段_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/臺東縣南迴公段_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/臺東縣南迴公段_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/臺東縣南迴公段_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/臺東縣南迴公段_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/臺東縣南迴公段_106.jpg"
         ]
     },
     {
         name: "嘉義布袋",
         latlng: [23.387, 120.1277],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/嘉義布袋_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/嘉義布袋_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/嘉義布袋_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/嘉義布袋_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/嘉義布袋_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/嘉義布袋_106.jpg"
         ]
     },
     {
         name: "高雄興達港",
         latlng: [22.8382, 120.1989],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/高雄興達港_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/高雄興達港_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/高雄興達港_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/高雄興達港_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/高雄興達港_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/高雄興達港_106.jpg"
         ]
     },
     {
         name: "花蓮溪口",
         latlng: [23.9722, 121.6291],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/花蓮溪口_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/花蓮溪口_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/花蓮溪口_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/花蓮溪口_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/花蓮溪口_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/花蓮溪口_106.jpg"
         ]
     },
     {
         name: "台中港及彰濱",
         latlng: [24.154437, 120.43286],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/台中港及彰濱_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/台中港及彰濱_104.jpg",
-            "dist/img/GIS_image_data/hot_point/106/台中港及彰濱_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/台中港及彰濱_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/台中港及彰濱_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/台中港及彰濱_106.jpg"
         ]
     },
     {
         name: "濁水溪口",
         latlng: [23.671912, 120.146396],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/濁水溪口_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/濁水溪口_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/濁水溪口_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/濁水溪口_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/濁水溪口_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/濁水溪口_106.jpg"
         ]
     },
     {
         name: "桃園觀音、新屋",
         latlng: [ 25.0169, 121.0473],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/桃園觀音、新屋_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/桃園觀音、新屋_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/桃園觀音、新屋_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/桃園觀音、新屋_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/桃園觀音、新屋_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/桃園觀音、新屋_106.jpg"
         ]
     },
     {
         name: "台南七股",
         latlng: [ 23.1444, 120.0533],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/台南七股_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/台南七股_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/台南七股_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/台南七股_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/台南七股_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/台南七股_106.jpg"
         ]
     },
     {
         name: "高雄左營及旗津",
         latlng: [ 22.6202, 120.3134],
         image:[
-            "dist/img/GIS_image_data/hot_point/104/高雄左營及旗津_104.jpg",
-            "dist/img/GIS_image_data/hot_point/105/高雄左營及旗津_105.jpg",
-            "dist/img/GIS_image_data/hot_point/106/高雄左營及旗津_106.jpg"
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/104/高雄左營及旗津_104.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/105/高雄左營及旗津_105.jpg",
+            "GEOJSON_file/before/coastal/img/GIS_image_data/hot_point/106/高雄左營及旗津_106.jpg"
         ]
     }
 ];
